9d46c9bda4bfa0f91162646e39ece82294269169
VaanaCZ <vaana@centrum.cz>
2022-06-24 23:24:43 +0200

    Finster - Fully functional texture loading

--- z_obj/space.hpp
+++ z_obj/space.hpp
@@ -2,8 +2,61 @@
 
 #include <fstream>
 
-bool ReadGFX(const wchar_t* filename,
-	uint16_t* width, uint16_t* height, unsigned char** buffer, bool convertToRGB888)
+bool ReadTexPool(const wchar_t* filename, const char* texture,
+	char* palette, char** buffer)
+{
+	std::ifstream file(filename, std::ios::binary);
+
+	if (file.fail())
+		return false;
+
+	file.seekg(0, std::ios::end);
+	uint64_t filesize = file.tellg();
+	file.seekg(0, std::ios::beg);
+
+	// Find the correct texture
+	while (true)
+	{
+		unsigned char nameLength;
+		file.read((char*)&nameLength, sizeof(nameLength));
+
+		if (nameLength == 0)
+			continue;
+
+		char* name = new char[nameLength + 1];
+		name[nameLength] = NULL;
+
+		file.read(name, nameLength);
+
+		bool found = strcmp(name, texture) == 0;
+
+		delete[] name;
+
+		if (found)
+		{
+			*buffer = new char[12288];
+			file.read(*buffer, 4096);
+
+			for (uint32_t i = 4096; i-- > 0; )
+			{
+				unsigned char pixel = (*buffer)[i];
+
+				(*buffer)[i * 3]		= palette[pixel * 3];
+				(*buffer)[i * 3 + 1]	= palette[pixel * 3 + 1];
+				(*buffer)[i * 3 + 2]	= palette[pixel * 3 + 2];
+			}
+
+			return true;
+		}
+
+		file.ignore(4127);
+	}
+
+	return false;
+}
+
+bool ReadGFX(const wchar_t* filename, char* palette, 
+	uint16_t* width, uint16_t* height, unsigned char** buffer)
 {
 	std::ifstream file(filename, std::ios::binary);
 
@@ -28,44 +81,16 @@ bool ReadGFX(const wchar_t* filename,
 	if (filesize < sizeof(res) + datasize)
 		return false;
 
-	*buffer = new unsigned char[datasize * (convertToRGB888 ? 3 : 1)];
+	*buffer = new unsigned char[datasize * 3];
 	file.read((char*)*buffer, datasize);
 
-	if (convertToRGB888)
+	for (uint32_t i = datasize; i-- > 0; )
 	{
-		for (uint32_t i = datasize; i-- > 0; )
-		{
-			char pixel = (*buffer)[i];
-
-			// A smarter programmer would make a lookup table ;)
-
-
-			struct test
-			{
-				unsigned char r : 3;
-				unsigned char g : 3;
-				unsigned char b : 2;
-			};
-
-			test t = *(test*)&pixel;
-
-			//unsigned char r = (pixel & 0xe0) >> 5;
-			//unsigned char g = (pixel & 0x1c) >> 2;
-			//unsigned char b = pixel & 0x03;
-			
-			unsigned char r = t.r;
-			unsigned char g = t.g;
-			unsigned char b = t.b;
-
-			r = ceil(255.0f / 7.0f * r);
-			g = ceil(255.0f / 7.0f * g);
-			b = ceil(255.0f / 3.0f * b);
-
-			(*buffer)[i * 3]		= b;
-			(*buffer)[i * 3 + 1]	= g;
-			(*buffer)[i * 3 + 2]	= r;
-
-		}
+		unsigned char pixel = (*buffer)[i];
+		
+		(*buffer)[i * 3]		= palette[pixel * 3];
+		(*buffer)[i * 3 + 1]	= palette[pixel * 3 + 1];
+		(*buffer)[i * 3 + 2]	= palette[pixel * 3 + 2];
 	}
 
 	*width	= res[0];
@@ -74,6 +99,25 @@ bool ReadGFX(const wchar_t* filename,
 	return true;
 }
 
+bool ReadPAL(const wchar_t* filename, char** buffer)
+{
+	std::ifstream file(filename, std::ios::binary);
+
+	if (file.fail())
+		return false;
+
+	file.seekg(0, std::ios::end);
+	uint64_t filesize = file.tellg();
+	file.seekg(0, std::ios::beg);
+
+	if (filesize != 768)
+		return false;
+
+	*buffer = new char[768];
+	file.read(*buffer, 768);
+}
+
+#pragma pack(push, 1)
 struct VOB3D_Vertex
 {
 	int16_t x, y, z;
@@ -83,9 +127,11 @@ struct VOB3D_Face
 {
 	int16_t v1, v2, v3, v4; // If v4 is -1, this is a tri, otherwise its a quad
 
-	unsigned char	nameLength;
-	char			name[15];
+	unsigned char	texutreNameLength;
+	char			textureName[8];
+	char			reserved[7];
 };
+#pragma pack(pop)
 
 bool ReadV3D(const wchar_t* filename,
 	VOB3D_Vertex** verts, uint16_t* vertCount,
